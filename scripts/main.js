02ab1d4a2040af7e970bd53cecb6e6f8b4f1fa2d
Lucas Rocha <lr359508@gmail.com>
2023-05-28 10:09:58 -0300

    Zuando.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
 let canvas = document.querySelector('#canvas')
 let ctx = canvas.getContext('2d')
+
+setInterval(fazer = () => {
+    ctx.font = '20px Arial'
+    ctx.moveTo(0,0)
+},50)
+
+let corpo = document.body
+let result = document.createElement('p')
+result.innerText = 'Teste'
+result.style.textAlign = 'center'
+result.style.fontSize = '2em'
+corpo.appendChild(result)
+
+canvas.addEventListener('mousemove',(event) => {
+    let x = event.pageX
+    let y = event.pageY
+    result.innerText = `x: ${x} y: ${y}`
+})
