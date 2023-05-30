a8fe30b6710f58c08554f8136509d187f0eb9c2e
Lucas Rocha <lr359508@gmail.com>
2023-05-30 11:44:16 -0300

    Adicionada uma imagem mais completa.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let ctx = canvas.getContext('2d')
 const baralho = document.createElement('img')
 baralho.src = 'imagens/baralho_otim.png'
 
-const card_cutx=62,card_cuty=87
+const card_cutx=80,card_cuty=124
 
 class Card{
     constructor(card_x,card_y,card_rec_posx,card_rec_posy,card_remx,card_remy){
@@ -14,13 +14,13 @@ class Card{
         this.card_rec_posy = card_rec_posy
         this.card_x = card_x
         this.card_x = card_y
-        this.card_remx
-        this.card_remy
+        this.card_remx = card_remx
+        this.card_remy = card_remy
     }
 }
 
 //drawImage(imagem,posx recorte, posy recorte, tamx recorte, tamy recorte, coord x, coord y, rem x, rem y)
 
 baralho.addEventListener('load',()=>{
-    ctx.drawImage(baralho,0,0,card_cutx,card_cuty,30,30,100,100)  
+    ctx.drawImage(baralho,0,0,card_cutx,card_cuty,20,20,40,62)  
 })
